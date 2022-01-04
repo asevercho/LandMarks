bc2fa55652941ae2479bd5f50f5892ff0a3e0b54
CHO <asevercho86@gmail.com>
2022-01-04 22:17:09 +0900

    first commit

--- LandMarks/LandMarks/ContentView.swift
+++ LandMarks/LandMarks/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Turtle Rock")
+        Text("Turtle Rock first Commit")
             .font(.title)
             
     }
