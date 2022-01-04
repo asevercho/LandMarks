63a528e6656dab4295c4c85c62965b5578cd85ed
asevercho86 <97018512+asevercho@users.noreply.github.com>
2022-01-04 21:43:12 +0900

    Add files via upload

---
+++ LandMarks/LandMarks/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  LandMarks
+//
+//  Created by DEOKSHIN CHO on 2021/12/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Turtle Rock")
+            .font(.title)
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LandMarks/LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by DEOKSHIN CHO on 2021/12/29.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
