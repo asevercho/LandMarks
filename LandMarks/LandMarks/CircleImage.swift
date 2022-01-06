2ac09761433a54d1c63cb6d9d1c240ba294ebfcf
CHO <asevercho86@gmail.com>
2022-01-06 22:35:04 +0900

    SwiftUI Tutorials Ch1 - Section 5 Use SwiftUI Views From Other Frameworks

---
+++ LandMarks/LandMarks/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  LandMarks
+//
+//  Created by DEOKSHIN CHO on 2022/01/06.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay{
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
