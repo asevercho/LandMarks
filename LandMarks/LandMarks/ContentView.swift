ef68f12ab7c27db534aa797bc1f2d8c82d149bd6
CHO <asevercho86@gmail.com>
2022-01-08 17:37:09 +0900

    Ch1

--- LandMarks/LandMarks/ContentView.swift
+++ LandMarks/LandMarks/ContentView.swift
@@ -9,18 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("first Commit")
-                    .font(.subheadline)
-                Spacer()
-                Text("second Commit")
-                    .font(.subheadline)
+        VStack {
+            MapView()
+//                .ignoresSafeArea()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/-130.0/*@END_MENU_TOKEN@*/)
+                .padding(.bottom, -130.0)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
             }
+            .padding()
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
